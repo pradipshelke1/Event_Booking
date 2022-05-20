e9a1787f0d11dd108bfee32ed8641443083a244b
Pradip Shelke <pradipshelke12440@gmail.com>
2022-05-20 10:08:52 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>hello</h3>
+      {/* <i className='fa fa-user-circle' />
+      <button className='btn btn-warning btn-sm'>Login</button> */}
+    </div>
+  );
+}
+
+export default App; 
